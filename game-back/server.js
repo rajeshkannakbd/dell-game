7618b6f067ee8d8c1f8176c4902da0b90456bbb7
rajeshkanna <rajeshkannakbd@gmail.com>
2026-01-05 11:02:42 +0530

    last fix

--- game-back/server.js
+++ game-back/server.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://dell-game-lingesh-server.onrender.com"
+  "https://dell-game-lingesh-frontend.onrender.com"
 ];
 
 app.use(
@@ -165,7 +165,7 @@ app.get("/leaderboard", async (req, res) => {
 });
 
 /* ---------------- Start ---------------- */
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running on ${PORT} 🚀`)
 );
